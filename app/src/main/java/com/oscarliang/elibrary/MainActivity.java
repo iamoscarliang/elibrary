e77ea4e3dd747e6ce16505994267b762a7f74f6d
Oscar <a7757729@gmail.com>
2023-12-30 18:57:42 +0800

    Remove actionbar

--- app/src/main/java/com/oscarliang/elibrary/MainActivity.java
+++ app/src/main/java/com/oscarliang/elibrary/MainActivity.java
@@ -3,10 +3,6 @@ package com.oscarliang.elibrary;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.oscarliang.elibrary.di.Injectable;
 
@@ -28,11 +24,6 @@ public class MainActivity extends AppCompatActivity implements Injectable, HasAn
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavController navController = navHostFragment.getNavController();
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupWithNavController(findViewById(R.id.toolbar), navController, appBarConfiguration);
     }
 
     @Override
